0073d8aa320facccd116a102a5b4f00cfe317382
Nikita <nik.gorbunov.02022002@mail.ru>
2022-09-15 23:35:05 +0300

    эвм 2 лаба

--- ЛиПО/Лабы/Lab_1/Project1/Source.cpp
+++ ЛиПО/Лабы/Lab_1/Project1/Source.cpp
@@ -5,11 +5,17 @@
 
 using namespace std;
 
+
+bool SyntaxCheck(char* buf);
+
 int main() {
     setlocale(LC_ALL, "ru");
    
     fstream file; // создаем объект класса ifstream
-    file.open("D:\\универ\\ЛиПО\\!Лабораторные\\Lab_1\\files\\prog.txt"); // открываем файл
+    // Пример:
+    // D:\\универ\\ЛиПО\\!Лабораторные\\Lab_1\\files\\prog.txt"
+    string filePath = "D:\\Универ\\PGU\\ЛиПО\\Лабы\\Lab_1\\files\\prog.txt";
+    file.open(filePath); // открываем файл
     vector<string> v;
     
     
@@ -20,15 +26,25 @@ int main() {
     }
     else
     {
-        char ch;                //символ, считываемый из файла
-        int pos = 0;            //позиция последнего символа в собранной строке
-        int number = 0;         //номер строки вектора
-        while (file.get(ch)) {
-
-            cout << ch;
-        }
+        int length = file.tellg();
+        char* buffer = new char[length];
+        file.read(buffer, length);
         file.close();
-
+        bool result = SyntaxCheck(buffer);
         
     }
+}
+
+
+
+bool SyntaxCheck(char * buf) 
+{
+    char ch;                //символ, считываемый из файла
+    int pos = 0;            //позиция последнего символа в собранной строке
+    int number = 0;         //номер строки вектора
+    for (int i = 0; i < sizeof(buf); i++)
+    {
+        cout << buf[i];
+    }
+    return true;
 }
