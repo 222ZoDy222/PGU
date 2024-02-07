25937bf8fb34406c70474fb57ad4cb0f47e3be47
Nikita <nik.gorbunov.02022002@mail.ru>
2024-02-08 00:51:57 +0300

    тестирование и тарам парам

---
+++ 8 семестр/ТЕстирование/1 лаба/Lab1/Menues/InputMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1.Menues
+{
+    
+    public class InputMenu : MenuUI
+    {
+
+        public InputMenu(GroupBox group)
+        {
+            currentGroup = group;
+        }
+
+
+
+
+
+
+    }
+}
