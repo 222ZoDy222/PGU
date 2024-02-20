80e9f138cbae608b174391db44f00e6ac3753aee
Nikita <nik.gorbunov.02022002@mail.ru>
2024-02-21 00:53:56 +0300

    чуть тестирования

---
+++ 8 семестр/Тестирование/Project/ProjectArrayShifter/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace ProjectArrayShifter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputArraySize = new System.Windows.Forms.TextBox();
+            this.buttonArraySize = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ArrayInputField = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelCountElements = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputArraySize
+            // 
+            this.inputArraySize.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputArraySize.Location = new System.Drawing.Point(70, 55);
+            this.inputArraySize.Name = "inputArraySize";
+            this.inputArraySize.Size = new System.Drawing.Size(75, 29);
+            this.inputArraySize.TabIndex = 0;
+            this.inputArraySize.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonArraySize
+            // 
+            this.buttonArraySize.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonArraySize.Location = new System.Drawing.Point(61, 101);
+            this.buttonArraySize.Name = "buttonArraySize";
+            this.buttonArraySize.Size = new System.Drawing.Size(95, 32);
+            this.buttonArraySize.TabIndex = 1;
+            this.buttonArraySize.Text = "Ввод";
+            this.buttonArraySize.UseVisualStyleBackColor = true;
+            this.buttonArraySize.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(455, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Сдвинуть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ArrayInputField
+            // 
+            this.ArrayInputField.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ArrayInputField.Location = new System.Drawing.Point(261, 51);
+            this.ArrayInputField.Name = "ArrayInputField";
+            this.ArrayInputField.Size = new System.Drawing.Size(524, 29);
+            this.ArrayInputField.TabIndex = 2;
+            this.ArrayInputField.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.buttonArraySize);
+            this.panel1.Controls.Add(this.labelCountElements);
+            this.panel1.Controls.Add(this.inputArraySize);
+            this.panel1.Location = new System.Drawing.Point(15, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(255, 151);
+            this.panel1.TabIndex = 5;
+            // 
+            // labelCountElements
+            // 
+            this.labelCountElements.AutoSize = true;
+            this.labelCountElements.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCountElements.Location = new System.Drawing.Point(-2, 24);
+            this.labelCountElements.Name = "labelCountElements";
+            this.labelCountElements.Size = new System.Drawing.Size(254, 24);
+            this.labelCountElements.TabIndex = 4;
+            this.labelCountElements.Text = "Кол-во элементов массива";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.ArrayInputField);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(15, 15);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(849, 453);
+            this.panel2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(261, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(312, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "вещественные числа через точку";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(382, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(236, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "элементов через пробел";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(346, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "15";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(261, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 480);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputArraySize;
+        private System.Windows.Forms.Button buttonArraySize;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox ArrayInputField;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelCountElements;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 8 семестр/Тестирование/Project/ProjectArrayShifter/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectArrayShifter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private const int minSizeArray = 2;
+        private const int maxSizeArray = 15;
+
+
+        private int m_arraySize = 2;
+        public int ArraySize
+        {
+            get => m_arraySize;
+
+            private set
+            {
+                if (value < minSizeArray)
+                {
+                    // TODO: Введено значение меньше 15
+                    m_arraySize = minSizeArray;
+                }
+                else if (value > maxSizeArray)
+                {
+                    // TODO: Введено значение больше 15
+                    m_arraySize = maxSizeArray;
+                }
+                else
+                {
+                    
+                    m_arraySize = value;
+                }
+
+                inputArraySize.Text = m_arraySize.ToString();
+            }
+        }
+
+
+        private void inputArraySize_OnChanged()
+        {
+
+            if (int.TryParse(inputArraySize.Text, out int sizeArray))
+            {
+
+                ArraySize = sizeArray;
+
+
+            }
+            else
+            {
+                // TODO: Введено неверное значение
+                inputArraySize.Text = "";
+            }
+        }
+
+        
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            inputArraySize_OnChanged();
+        }
+
+        /// <summary>
+        /// Кнопка Сдвинуть
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ShiftArray();
+        }
+
+
+        private void ShiftArray()
+        {
+            // проверить верные ли значения в InputField
+        }
+
+
+
+
+
+
+        private bool CheckArrayField()
+        {
+            var value = ArrayInputField.Text;
+
+
+        }
+
+    }
+}
